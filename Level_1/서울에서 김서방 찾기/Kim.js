4333105ba23504da46b45cfc1e287dcabc744668
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-05-30 20:18:42 +0900

    Create Kim.js

---
+++ Level_1/서울에서 김서방 찾기/Kim.js
@@ -0,0 +1,8 @@
+function solution(seoul) {
+    let answer = '';
+    let cnt = 0;
+    for(let i = 0; i < seoul.length; i++){
+        if(seoul[i] == "Kim") answer = "김서방은 " + i +"에 있다";
+    }
+    return answer;
+}
