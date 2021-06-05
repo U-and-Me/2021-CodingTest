bf73362a0222171247921ae5bbe9b5fe9c93de26
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-06-05 22:14:09 +0900

    Create Completion.java

---
+++ Level_1/완주하지 못한 선수/Completion.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        int i;
+        int c_len = completion.length;
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(i = 0; i < c_len; i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                return answer;
+            }
+        }
+        answer = participant[i];
+        return answer;
+    }
+}
