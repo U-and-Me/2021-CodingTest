5c6c52c448e2a32083e86951036cdde044743faa
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-06-10 21:22:17 +0900

    Create Prime.java

---
+++ Level_1/소수 만들기/Prime.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int len = nums.length;
+
+        for(int i = 0; i < len; i++){
+            for(int j = i+1; j < len; j++){
+                for(int k = j+1; k < len; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)) answer++;
+                }
+            }
+        }        
+
+        return answer;
+    }
+    
+    public boolean isPrime(int num){
+        for(int i = 2; i < num; i++)
+            if(num % i == 0) return false;
+        return true;
+    }
+}
