4391754d8b298728f08b75fa20681a18db86cc35
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-06-28 19:48:13 +0900

    Create Collatz.cpp

---
+++ Level_1/콜라츠 추측/Collatz.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long num2 = (long)num;
+    while(num2 != 1){
+        if(num2 % 2 == 0) num2 /= 2;
+        else num2 = num2 * 3 + 1;
+        answer++;
+        if(answer >= 500) return -1;
+    }
+    return answer;
+}
