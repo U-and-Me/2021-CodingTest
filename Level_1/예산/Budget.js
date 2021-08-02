36c222fc0aa11c0c833f134eca22c7d91dc2e6e6
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-08-02 14:47:35 +0900

    Create Budget.js

---
+++ Level_1/예산/Budget.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    let answer = 0;
+    
+    d = d.sort((a, b) => a - b)
+    
+    for(let i = 0; i < d.length; i++){
+        budget -= d[i];
+        if(budget < 0) break;
+        answer++;
+    }
+    
+    return answer;
+}
