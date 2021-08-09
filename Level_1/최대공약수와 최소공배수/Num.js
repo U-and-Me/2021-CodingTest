1b65c4ee0b65ca3e9a5faf49cef80de93c1bc9ee
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-08-09 14:54:56 +0900

    Create Num.js

---
+++ Level_1/최대공약수와 최소공배수/Num.js
@@ -0,0 +1,11 @@
+function solution(n, m) {
+    var answer = [];
+    
+    for(let i = 1; i <= n || i <= m; i++)
+        if(n % i == 0 && m % i == 0)
+            answer[0] = i;
+
+    answer[1] = (n * m) / answer[0];
+    
+    return answer;
+}
