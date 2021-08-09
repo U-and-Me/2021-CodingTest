768e4fc3aacd1a7c690f01a5de767ea019aa0a62
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-08-09 14:55:12 +0900

    Create Num.cpp

---
+++ Level_1/최대공약수와 최소공배수/Num.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer(2);
+    
+    for(int i = 1; i <= n || i <= m; i++){
+        if(n % i == 0 && m % i == 0)
+            answer[0] = i;
+    }
+    
+    answer[1] = (n * m) / answer[0];
+    
+    return answer;
+}
