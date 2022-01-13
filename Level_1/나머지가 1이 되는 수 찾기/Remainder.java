684a479bdb3aa082995bf6c086c199ff9d3cba0e
Kim Yu Na <yunasun0920@gmail.com>
2022-01-13 22:29:23 +0900

    Create Remainder.java

---
+++ Level_1/나머지가 1이 되는 수 찾기/Remainder.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n;
+        
+        for(int i = n - 1; i > 0; i--){
+            if(n % i == 1 && answer > i){
+                answer = i;
+            }
+        }
+                
+        return answer;
+    }
+}
